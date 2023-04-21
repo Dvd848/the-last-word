72f25eac677a09473bd5878beaec5fda1418b788
Dvd848 <Dvd848@users.noreply.github.com>
2023-04-22 00:01:51 +0300

    Bundle dawg code separately

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,44 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    entry: './src/app.ts',
+    entry: {
+      dawg: ['./src/dawg/dawgs.ts', './src/dawg/units.ts', './src/dawg/wrapper.ts'],
+      app: {
+        import: './src/app.ts',
+        dependOn: 'dawg'
+      }
+    },
     watchOptions: {
         aggregateTimeout: 200,
         poll: 1000,
         ignored: /node_modules/,
       },
     output: {
-      filename: 'bundle.js',
+      filename: '[name].bundle.js',
+      chunkFilename: '[name].chunk.js',
       path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            output: {
+              comments: false,
+            },
+          },
+          exclude: /dawg\.bundle\.js/,
+          extractComments: false,
+        }),
+      ],
     },
     devServer: {
         static: path.resolve(__dirname),
         devMiddleware: {
-            publicPath: '/dist/'
+            publicPath: '/dist/',
+            writeToDisk: false,
          },
         port: 8080,
         hot: false
