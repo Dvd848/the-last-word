e17562640b9fac7b9de8e794c7f757fcbc2dc93d
Dvd848 <Dvd848@users.noreply.github.com>
2025-07-11 18:51:11 +0300

    Update dependencies

--- dist/dawg.bundle.js
+++ dist/dawg.bundle.js
@@ -1,186 +1,15 @@
 "use strict";
-(self["webpackChunkthe_last_word"] = self["webpackChunkthe_last_word"] || []).push([[4],{
+(self["webpackChunkthe_last_word"] = self["webpackChunkthe_last_word"] || []).push([[284],{
 
-/***/ 701:
+/***/ 171:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "$": () => (/* binding */ CompletionDAWG)
+/* harmony export */   AP: () => (/* binding */ Completer),
+/* harmony export */   Lk: () => (/* binding */ Guide),
+/* harmony export */   bT: () => (/* binding */ Dictionary)
 /* harmony export */ });
-/* harmony import */ var _dawg_wrapper__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(765);
-/*
-This code was ported from Python to Javascript based on reference code from:
-https://github.com/pytries/DAWG-Python/
-
-The original code was licensed under MIT license:
-
-    Copyright (c) Mikhail Korobov, 2012
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is furnished
-    to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-    INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
-    A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
-    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-class CompletionDAWG {
-    constructor() {
-        this.dct = null;
-        this.guide = null;
-    }
-    keys(prefix = "") {
-        const encoder = new TextEncoder();
-        const decoder = new TextDecoder("utf-8");
-        const b_prefix = encoder.encode(prefix);
-        const res = [];
-        if ((this.dct == null) || (this.guide == null)) {
-            throw "Dictionary must be loaded first!";
-        }
-        const index = this.dct.follow_bytes(b_prefix, this.dct.ROOT);
-        if (index === null) {
-            return res;
-        }
-        const completer = new _dawg_wrapper__WEBPACK_IMPORTED_MODULE_0__/* .Completer */ .XB(this.dct, this.guide);
-        completer.start(index, b_prefix);
-        while (completer.next()) {
-            let key = decoder.decode(new Uint8Array(completer.key));
-            res.push(key);
-        }
-        return res;
-    }
-    edges(prefix = "") {
-        const encoder = new TextEncoder();
-        const decoder = new TextDecoder("utf-8");
-        const b_prefix = encoder.encode(prefix);
-        const res = [];
-        if ((this.dct == null) || (this.guide == null)) {
-            throw "Dictionary must be loaded first!";
-        }
-        const index = this.dct.follow_bytes(b_prefix, this.dct.ROOT);
-        if (index === null) {
-            return res;
-        }
-        const completer = new _dawg_wrapper__WEBPACK_IMPORTED_MODULE_0__/* .Completer */ .XB(this.dct, this.guide);
-        if (!completer.start_edges(index, b_prefix)) {
-            return res;
-        }
-        let key = decoder.decode(new Uint8Array(completer.key));
-        res.push(key.slice(-1));
-        while (completer.next_edge()) {
-            key = decoder.decode(new Uint8Array(completer.key));
-            res.push(key.slice(-1));
-        }
-        return res;
-    }
-    contains(key) {
-        const encoder = new TextEncoder();
-        const b_key = encoder.encode(key);
-        if ((this.dct == null) || (this.guide == null)) {
-            throw "Dictionary must be loaded first!";
-        }
-        return this.dct.contains(b_key);
-    }
-    load(raw_bytes) {
-        this.dct = new _dawg_wrapper__WEBPACK_IMPORTED_MODULE_0__/* .Dictionary */ .Xy();
-        this.guide = new _dawg_wrapper__WEBPACK_IMPORTED_MODULE_0__/* .Guide */ .fD();
-        this.dct.read(raw_bytes);
-        this.guide.read(raw_bytes);
-        return this;
-    }
-}
-
-
-/***/ }),
-
-/***/ 163:
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "PS": () => (/* binding */ label),
-/* harmony export */   "S3": () => (/* binding */ value),
-/* harmony export */   "Wf": () => (/* binding */ PRECISION_MASK),
-/* harmony export */   "cv": () => (/* binding */ offset),
-/* harmony export */   "yI": () => (/* binding */ has_leaf)
-/* harmony export */ });
-/* unused harmony exports OFFSET_MAX, IS_LEAF_BIT, HAS_LEAF_BIT, EXTENSION_BIT */
-/*
-This code was ported from Python to Javascript based on reference code from:
-https://github.com/pytries/DAWG-Python/
-
-The original code was licensed under MIT license:
-
-    Copyright (c) Mikhail Korobov, 2012
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is furnished
-    to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-    INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
-    A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
-    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-const PRECISION_MASK = 0xFFFFFFFF;
-const OFFSET_MAX = (/* unused pure expression or super */ null && (1 << 21));
-const IS_LEAF_BIT = 1 << 31;
-const HAS_LEAF_BIT = 1 << 8;
-const EXTENSION_BIT = 1 << 9;
-/**
- * Check if a unit has a leaf as a child or not.
- */
-function has_leaf(base, _mask = HAS_LEAF_BIT) {
-    return (base & _mask) ? true : false;
-}
-/**
- * Check if a unit corresponds to a leaf or not.
- */
-function value(base, _mask = ~IS_LEAF_BIT & PRECISION_MASK) {
-    return base & _mask;
-}
-/**
- * Read a label with a leaf flag from a non-leaf unit.
- */
-function label(base, _mask = IS_LEAF_BIT | 0xFF) {
-    return base & _mask;
-}
-/**
- * Read an offset to child units from a non-leaf unit.
- */
-function offset(base) {
-    return ((base >> 10) << ((base & EXTENSION_BIT) >> 6)) & PRECISION_MASK;
-}
-
-
-/***/ }),
-
-/***/ 765:
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "XB": () => (/* binding */ Completer),
-/* harmony export */   "Xy": () => (/* binding */ Dictionary),
-/* harmony export */   "fD": () => (/* binding */ Guide)
-/* harmony export */ });
-/* harmony import */ var _units__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(163);
+/* harmony import */ var _units__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(519);
 /*
 This code was ported from Python to Javascript based on reference code from:
 https://github.com/pytries/DAWG-Python/
@@ -222,7 +51,7 @@ class Dictionary {
         if (this._units == null) {
             return false;
         }
-        return _units__WEBPACK_IMPORTED_MODULE_0__/* .has_leaf */ .yI(this._units[index]);
+        return _units__WEBPACK_IMPORTED_MODULE_0__/* .has_leaf */ .e4(this._units[index]);
     }
     /**
      * Gets a value from a given index.
@@ -231,9 +60,9 @@ class Dictionary {
         if (this._units == null) {
             throw "Error: _units is null!";
         }
-        const offset = _units__WEBPACK_IMPORTED_MODULE_0__/* .offset */ .cv(this._units[index]);
-        const value_index = (index ^ offset) & _units__WEBPACK_IMPORTED_MODULE_0__/* .PRECISION_MASK */ .Wf;
-        return _units__WEBPACK_IMPORTED_MODULE_0__/* .value */ .S3(this._units[value_index]);
+        const offset = _units__WEBPACK_IMPORTED_MODULE_0__/* .offset */ .cY(this._units[index]);
+        const value_index = (index ^ offset) & _units__WEBPACK_IMPORTED_MODULE_0__/* .PRECISION_MASK */ .ym;
+        return _units__WEBPACK_IMPORTED_MODULE_0__/* .value */ .Uq(this._units[value_index]);
     }
     /**
      * Reads a dictionary from an input stream.
@@ -273,9 +102,9 @@ class Dictionary {
         if (this._units == null) {
             throw "Error: _units is null!";
         }
-        const offset = _units__WEBPACK_IMPORTED_MODULE_0__/* .offset */ .cv(this._units[index]);
-        const next_index = (index ^ offset ^ label) & _units__WEBPACK_IMPORTED_MODULE_0__/* .PRECISION_MASK */ .Wf;
-        if (_units__WEBPACK_IMPORTED_MODULE_0__/* .label */ .PS(this._units[next_index]) != label) {
+        const offset = _units__WEBPACK_IMPORTED_MODULE_0__/* .offset */ .cY(this._units[index]);
+        const next_index = (index ^ offset ^ label) & _units__WEBPACK_IMPORTED_MODULE_0__/* .PRECISION_MASK */ .ym;
+        if (_units__WEBPACK_IMPORTED_MODULE_0__/* .label */ .Pf(this._units[next_index]) != label) {
             return null;
         }
         return next_index;
@@ -446,11 +275,182 @@ class Completer {
 }
 
 
+/***/ }),
+
+/***/ 306:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   E: () => (/* binding */ CompletionDAWG)
+/* harmony export */ });
+/* harmony import */ var _dawg_wrapper__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(171);
+/*
+This code was ported from Python to Javascript based on reference code from:
+https://github.com/pytries/DAWG-Python/
+
+The original code was licensed under MIT license:
+
+    Copyright (c) Mikhail Korobov, 2012
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is furnished
+    to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+    INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+    A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+class CompletionDAWG {
+    constructor() {
+        this.dct = null;
+        this.guide = null;
+    }
+    keys(prefix = "") {
+        const encoder = new TextEncoder();
+        const decoder = new TextDecoder("utf-8");
+        const b_prefix = encoder.encode(prefix);
+        const res = [];
+        if ((this.dct == null) || (this.guide == null)) {
+            throw "Dictionary must be loaded first!";
+        }
+        const index = this.dct.follow_bytes(b_prefix, this.dct.ROOT);
+        if (index === null) {
+            return res;
+        }
+        const completer = new _dawg_wrapper__WEBPACK_IMPORTED_MODULE_0__/* .Completer */ .AP(this.dct, this.guide);
+        completer.start(index, b_prefix);
+        while (completer.next()) {
+            let key = decoder.decode(new Uint8Array(completer.key));
+            res.push(key);
+        }
+        return res;
+    }
+    edges(prefix = "") {
+        const encoder = new TextEncoder();
+        const decoder = new TextDecoder("utf-8");
+        const b_prefix = encoder.encode(prefix);
+        const res = [];
+        if ((this.dct == null) || (this.guide == null)) {
+            throw "Dictionary must be loaded first!";
+        }
+        const index = this.dct.follow_bytes(b_prefix, this.dct.ROOT);
+        if (index === null) {
+            return res;
+        }
+        const completer = new _dawg_wrapper__WEBPACK_IMPORTED_MODULE_0__/* .Completer */ .AP(this.dct, this.guide);
+        if (!completer.start_edges(index, b_prefix)) {
+            return res;
+        }
+        let key = decoder.decode(new Uint8Array(completer.key));
+        res.push(key.slice(-1));
+        while (completer.next_edge()) {
+            key = decoder.decode(new Uint8Array(completer.key));
+            res.push(key.slice(-1));
+        }
+        return res;
+    }
+    contains(key) {
+        const encoder = new TextEncoder();
+        const b_key = encoder.encode(key);
+        if ((this.dct == null) || (this.guide == null)) {
+            throw "Dictionary must be loaded first!";
+        }
+        return this.dct.contains(b_key);
+    }
+    load(raw_bytes) {
+        this.dct = new _dawg_wrapper__WEBPACK_IMPORTED_MODULE_0__/* .Dictionary */ .bT();
+        this.guide = new _dawg_wrapper__WEBPACK_IMPORTED_MODULE_0__/* .Guide */ .Lk();
+        this.dct.read(raw_bytes);
+        this.guide.read(raw_bytes);
+        return this;
+    }
+}
+
+
+/***/ }),
+
+/***/ 519:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Pf: () => (/* binding */ label),
+/* harmony export */   Uq: () => (/* binding */ value),
+/* harmony export */   cY: () => (/* binding */ offset),
+/* harmony export */   e4: () => (/* binding */ has_leaf),
+/* harmony export */   ym: () => (/* binding */ PRECISION_MASK)
+/* harmony export */ });
+/* unused harmony exports OFFSET_MAX, IS_LEAF_BIT, HAS_LEAF_BIT, EXTENSION_BIT */
+/*
+This code was ported from Python to Javascript based on reference code from:
+https://github.com/pytries/DAWG-Python/
+
+The original code was licensed under MIT license:
+
+    Copyright (c) Mikhail Korobov, 2012
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is furnished
+    to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+    INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+    A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+const PRECISION_MASK = 0xFFFFFFFF;
+const OFFSET_MAX = (/* unused pure expression or super */ null && (1 << 21));
+const IS_LEAF_BIT = 1 << 31;
+const HAS_LEAF_BIT = 1 << 8;
+const EXTENSION_BIT = 1 << 9;
+/**
+ * Check if a unit has a leaf as a child or not.
+ */
+function has_leaf(base, _mask = HAS_LEAF_BIT) {
+    return (base & _mask) ? true : false;
+}
+/**
+ * Check if a unit corresponds to a leaf or not.
+ */
+function value(base, _mask = ~IS_LEAF_BIT & PRECISION_MASK) {
+    return base & _mask;
+}
+/**
+ * Read a label with a leaf flag from a non-leaf unit.
+ */
+function label(base, _mask = IS_LEAF_BIT | 0xFF) {
+    return base & _mask;
+}
+/**
+ * Read an offset to child units from a non-leaf unit.
+ */
+function offset(base) {
+    return ((base >> 10) << ((base & EXTENSION_BIT) >> 6)) & PRECISION_MASK;
+}
+
+
 /***/ })
 
 },
 /******/ __webpack_require__ => { // webpackRuntimeModules
 /******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ var __webpack_exports__ = (__webpack_exec__(701), __webpack_exec__(163), __webpack_exec__(765));
+/******/ var __webpack_exports__ = (__webpack_exec__(306), __webpack_exec__(519), __webpack_exec__(171));
 /******/ }
 ]);
